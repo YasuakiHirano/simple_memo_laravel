2073e18b3875411461fba061b8bd18dc71c1fcc5
y.hirano <yasuakihappy@gmail.com>
2020-08-23 15:13:15 +0900

    メモ投稿機能実装

--- simple_memo_laravel/routes/web.php
+++ simple_memo_laravel/routes/web.php
@@ -19,10 +19,16 @@ Route::get('/', 'Auth\LoginController@showLoginForm')->name('login.index');
 Route::get('/user', 'Auth\RegisterController@showRegistrationForm')->name('user.index');
 Route::post('/user/register', 'Auth\RegisterController@register')->name('user.exec.register');
 
-Route::get('/memo', function() {
-    return view("memo");
-})->name('memo.index');
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/memo', 'MemoController@index')->name('memo.index');
+    Route::get('/memo/add', 'MemoController@add')->name('memo.add');
+    Route::post('/memo/delete', 'MemoController@delete')->name('memo.delete');
+    Route::post('/memo/update', 'MemoController@update')->name('memo.update');
+    Route::get('/memo/select', 'MemoController@select')->name('memo.select');
+});
+
 
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
