02ecd24aff6fed4641e1d803f9a50dd7a29e37b4
y.hirano <yasuakihappy@gmail.com>
2020-08-29 23:34:26 +0900

    [4章] 機能作成

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-## 作成中
+## simple memo laravel  
+laravelで作成したEvernote風メモアプリです。  
+nginx1.19.1 + php7.4-fpm + mysql5.7を使用しています。  
+
+## 起動する  
+docker-composeを使って、下記のコマンドで起動できます。  
+
+```
+docker-compose -f .docker_memo_laravel/docker-compose.yml up -d 
+```
+
+## 初期設定  
+起動後にデータベースの作成などの初期設定をします。  
+下記のコマンドで、dockerに入って  
+
+```
+docker-compose -f .docker_memo_laravel/docker-compose.yml exec php /bin/bash 
+```
+
+プロジェクトフォルダに移動した後に  
+
+```
+cd simple_memo_laravel/
+```
+
+下記を行います。  
+1. composer installでライブラリインストール  
+2. laravelの環境設定ファイル作成  
+3. appキーの生成  
+
+```
+composer install
+cp .env.example .env 
+php artisan key:generate
+```
+
+## データベースの設定  
+### データベース作成  
+http://localhost:8086でphpmyadminが開くので、そこからデータベースを作成します。  
+
+```
+CREATE DATABASE simple_memo_laravel DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+```
+
+### laravelでマイグレーション  
+dockerに入って、プロジェクトフォルダ配下でマイグレーションを実行します。  
+
+```
+docker-compose -f .docker_memo_laravel/docker-compose.yml exec php /bin/bash 
+cd simple_memo_laravel/
+php artisan migrate
+```
+
--- simple_memo_laravel/resources/views/auth/register.blade.php
+++ simple_memo_laravel/resources/views/auth/register.blade.php
@@ -2,9 +2,19 @@
 
 @section('content')
 <div class="d-flex align-items-center justify-content-center h-100">
-    <form method="get"  action="{{ route('memo.index') }}">
+    <form method="post" action="{{ route('user.exec.register') }}">
+        @csrf
         <div class="card rounded login-card-width shadow">
             <div class="card-body">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul class="mb-0 mt-0">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                 <div class="rounded-circle mx-auto border-gray border d-flex mt-3 icon-circle">
                     <img src="{{ asset('images/animal_stand_zou.png')  }}" class="w-75 mx-auto p-2" alt="icon"/>
                 </div>
@@ -17,13 +27,13 @@
                                 <span class="input-group-prepend">
                                     <span class="input-group-text"><i class="fas fa-file-signature"></i></span>
                                 </span>
-                            <input type="text" name="name" class="form-control" placeholder="ユーザー名" autocomplete="off" maxlength="255" />
+                            <input type="text" name="name" class="form-control" placeholder="ユーザー名" value="{{ old('name') }}" autocomplete="off" maxlength="255" />
                         </label>
                         <label class="input-group w-100">
                                 <span class="input-group-prepend">
                                     <span class="input-group-text"><i class="far fa-envelope"></i></span>
                                 </span>
-                            <input type="text" name="email" class="form-control" placeholder="メールアドレス" autocomplete="off" maxlength="255" />
+                            <input type="text" name="email" class="form-control" placeholder="メールアドレス" value="{{ old('email') }}" autocomplete="off" maxlength="255" />
                         </label>
                         <label class="input-group w-100">
                                 <span class="input-group-prepend">
